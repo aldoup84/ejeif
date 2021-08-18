0b26017a90e0dd8e294c252fd32667e50d354d6c
aldoup84 <aldo.up@culiacan.tecnm.mx>
2021-08-18 17:15:09 +0000

    prueba

---
+++ assignments/00Sistema/src/exercise.py
@@ -0,0 +1,22 @@
+import os
+def main():
+    os.system("clear")
+    a = float(input("Dame el valor de a:"))
+    b = float(input("Dame el valor de b:"))
+    c = float(input("Dame el valor de c:"))
+    d = float(input("Dame el valor de d:"))
+    e = float(input("Dame el valor de e:"))
+    f = float(input("Dame el valor de f:"))
+  
+    determinante = a*e - b*d
+    if determinante == 0:
+        print("El sistema no tiene solución...")
+    else:
+        x = (c * e - b * f) / determinante
+        y = (a * f - c * d) / determinante
+        print("La solucion al sistema de ecuaciones es:")
+        print(f"x={x}")
+        print(f"y={y}")
+
+if __name__=='__main__':
+    main()
